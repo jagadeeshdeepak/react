b7586215edd4ebf1f2a2e86d2b82903cf5001aef
Deepak Jagadeesh <djagadeesh@paypal.com>
2018-11-25 20:48:26 -0800

    how jsx is compiled

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +8,13 @@ class App extends Component {
         <h1>I'm a React App</h1>
       </div>
     );
+
+    // This is how the above code is interpreted by jsx.
+    // takes infinite args, but atleast 3
+    // first arg, a component
+    // second arg, config for the component
+    // third arg, any amount of children like whats nested inside div
+    //return React.createElement('div', null, React.createElement('h1', null, 'I\'m a react app!!'));
   }
 }
 
