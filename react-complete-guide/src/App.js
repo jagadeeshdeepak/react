6f5be570801e5d9eeee7f7f00e6746b855febdd0
Deepak Jagadeesh <djagadeesh@paypal.com>
2018-11-25 23:45:06 -0800

    introducing state in react component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  // this is an important property of a class
+  // state is a special thing which is managed within the component
+  // if something changes inside the state, then the dom is rerendered.
+  state = {
+    persons: [
+      {name:'Deepak', age: '31'},
+      {name:'Deeksha', age: '26'},
+      {name:'Karthik', age: '31'},
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>I'm a React App</h1>
-        <Person name="Deepak" age="31">He loves learning new tech</Person>
-        <Person name="Deeksha" age="26">She loves Skin Wars</Person>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>He loves learning new tech</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>She loves Skin Wars</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>He loves doing kelsa</Person>
       </div>
     );
 
