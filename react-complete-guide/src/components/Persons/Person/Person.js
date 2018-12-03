c513c0f4a3391c56a24d4aad39b90288062b2909
Deepak Jagadeesh <djagadeesh@paypal.com>
2018-12-03 01:42:00 -0800

    converting stateless components to stateful components to understand lifecycle hooks

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import css modules to get every style in it
 import classes from './Person.css';
 
-const person = (props) => {
+class Person extends Component {
   //return <p>I'm a { Math.floor(Math.random() * 30) } old person</p>;
   // STATE cannot be defined here
-  return (
-    <div className={classes.Person}>
-      <p onClick={props.click}>I'm {props.name} & {props.age} years old!</p>
-      <p>{props.children}</p>
-      <input onChange={props.changed}/>
-    </div>
-
-  );
+  render () {
+    return (
+      <div className={classes.Person}>
+        <p onClick={this.props.click}>I'm {this.props.name} & {this.props.age} years old!</p>
+        <p>{this.props.children}</p>
+        <input onChange={this.props.changed}/>
+      </div>
+    );
+  }
 }
 
-export default person;
+export default Person;
--- react-complete-guide/src/components/Persons/Persons.js
+++ react-complete-guide/src/components/Persons/Persons.js
@@ -1,22 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
 // oneline notation to return something
-const persons = (props) => (
-  // map the list of persons instead of repetitive code
-  // but for some we need events and for others we dont
-  // pass an index and specify which index wants events tagged in specific class
-  props.persons.map((person, index) => {
-    return <Person
-      name={person.name}
-      age={person.age}
-      // event with specific index on the element
-      click={() => props.clicked(index)}
-      // key property for react to track each and individual elements
-      key={person.id}
-      // update the state for the input field we typed
-      changed={(event) => props.changed(event, person.id)}/>
-  })
-);
+class Persons extends Component {
+  render() {
+    return this.props.persons.map((person, index) => {
+      return <Person
+        name={person.name}
+        age={person.age}
+        // event with specific index on the element
+        click={() => this.props.clicked(index)}
+        // key property for react to track each and individual elements
+        key={person.id}
+        // update the state for the input field we typed
+        changed={(event) => this.props.changed(event, person.id)}/>
+    })
+  }
+}
 
-export default persons;
+export default Persons;
